9c0e853c58e70863816c0cac27b7eeb773ae1949
Brann lee <ras301@naver.com>
2023-03-08 19:46:53 +0900

    인트로 추가 수정

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,25 @@
-$(function(){
-    $('body').addClass('on');
-
-    //loading
-    var _loadingtxt = '<p class="loading_text">Hi. My Name is <strong>Brann</strong>.</p>';
+ //로딩페이지
+function loading(){
+    var _loadingtxt = '<p class="loading_text">Hi. My Name is Brann.</p>';
     var _sec = 0;
+    for(var i = 0; i<9;i++){
+        $(_loadingtxt).appendTo('#loading');
+    }
+    //개별 요소 로딩 퍼센트에 따라 노출되도록 변경 고려할것
     setInterval(function(){
         if(_sec < 9){
-            $(_loadingtxt).appendTo('#loading');
-            if(_sec == 4)$('.loading_text:nth-child(5)').addClass('active');
+            $('.loading_text').eq(_sec).addClass('in');
         }else if(_sec == 9){
-            // $('.loading_text:nth-child(5)').delay(2000).addClass('active');
+            $('.loading_text:nth-child(5)').addClass('active');
         }else{
-            $('#loading').delay(3000).fadeOut();
-            return false;
+            $('#loading').delay(4000).fadeOut(800);
         }
         _sec++;
-        console.log(_sec);
     }
-    ,150);
+    ,200);
+}
+
+$(function(){
+    $('body').addClass('on');
+    loading();
 });
