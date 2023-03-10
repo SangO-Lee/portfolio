3e530e15c588835c1f477a55801f7560bf264121
Brann lee <ras301@naver.com>
2023-03-10 16:36:25 +0900

    main view 배경 추가, 스크롤바 스크립트 적용

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,6 @@
+var _windowTop = $(window).scrollTop();
+var _pageHeight = $('body').height() - $(window).height();
+ 
  //로딩페이지
 function loading(){
     var _loadingtxt = '<p class="loading_text">Hi. My Name is Brann.</p>';
@@ -14,7 +17,7 @@ function loading(){
         }else if(_loadIdx == 9){
             $('.loading_text:nth-child(5)').addClass('active');
         }else{
-            $('#loading').delay(4000).fadeOut(800, function(){
+            $('#loading').delay(2000).fadeOut(600, function(){
                 clearInterval(_addTxt);
                 //로드 완료 후 실행
                 $('body').addClass('on');
@@ -23,11 +26,42 @@ function loading(){
         console.log(_loadIdx);
         _loadIdx++;
     }
-    ,200);
-
-   
+    ,150);
 }
 
+//scrollbar
+function scrollBar (){
+    _windowTop = $(window).scrollTop();
+    _pageHeight = $('body').height() - $(window).height();
+    var _ratio = _windowTop / _pageHeight * 100;
+    
+    console.log(_ratio);
+    $('#scroll_bar .current_indicator').css('top', _ratio + '%');
+}
+
+//window scroll 시
+$(window).on('scroll',function(){
+    scrollBar();
+});
+
 $(function(){
-    loading();
+    // loading(); //로딩페이지
+    scrollBar();
+
+    //스크롤 버튼 
+    $('a[use="scroll"]').on('click',function(e){
+        var _target = $(this).attr('href');
+        var _targetTop = 0;
+        _target == '#' ? 0 : _targetTop = $(_target).offset().top;
+
+        $('body,html').animate({
+            scrollTop:0
+        },500);
+        
+        return false;
+    });
+    
+    //작업용
+    $('#loading').hide();
+    $('body').addClass('on');
 });
