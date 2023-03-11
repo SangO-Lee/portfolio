9414f4f4bc14c9ee85b5c878abeec0fdf06494df
Brann lee <ras301@naver.com>
2023-03-11 15:49:51 +0900

    scroll 이벤트 테스트

--- js/custom.js
+++ js/custom.js
@@ -37,6 +37,45 @@ function scrollBar (){
     $('#scroll_bar .current_indicator').css('top', _ratio + '%');
 }
 
+//scroll classing
+function scrollClassing(tgt){
+    var _windotHeight = $(window).height();
+    var _baseline = _windowTop + _windotHeight; //화면이 섹션 중간쯤 왔을때 이벤트 작동
+    var _target= tgt; //섹션에 해당하는 엘리멘트
+    var _count = _target.length; // 총 엘리멘트 갯수
+    var _lastIdx = _count + 1; //마지막 엘리멘트
+
+    // for(var i = 0 ; i < _count ; i++){
+            var _targetTop = _target.offset().top;
+            var _targetHeight = _target.height();
+            var _scrollProgress = (_baseline - _targetTop) / _targetHeight;
+            var _progessPercent = _scrollProgress * 100;
+            
+            $(_target).css({
+                'opacity': _scrollProgress * 0.7,
+                'background-size': (85 + _scrollProgress * 15) + '%'
+            });
+
+            // var _cond1 = _target.eq(i).offset().top;
+            // if(_target.eq(i).next().length) var _cond2 = _target.eq(i).next().offset().top;
+            // var _cond3 = _target.eq(0).offset().top;
+
+            // if(i != _lastIdx){
+            //     if(_baseline < _cond3){
+            //         _target.removeClass('active');
+            //     }else if(_baseline > _cond1 && _baseline < _cond2){
+            //         _target.eq(i).addClass('active').siblings().removeClass('active');
+            //     }
+            // }else if(i == _lastIdx){
+            //     if(_baseline > _cond1){ //마지막 엘리멘트 이후는 항상 마지막 엘리멘트에 active 추가
+            //         _target.eq(i).addClass('active').siblings().removeClass('active');
+            //     }
+            // }
+
+
+    // }
+}
+
 //window scroll 시
 $(window).on('scroll',function(){
     scrollBar();
@@ -47,6 +86,9 @@ $(window).on('scroll',function(){
     }else{
         $('#main_bg').hide();
     }
+
+    //history 
+    scrollClassing($('#history_content > article#at1'));
    
 });
 
